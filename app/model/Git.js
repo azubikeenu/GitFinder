c044295c6d4d1885f27cab02ac812ab8e5fcb4df
Enu Azubike <enuazubike88@gmail.com>
2021-06-15 13:54:32 +0100

    code cleanup

--- app/model/Git.js
+++ app/model/Git.js
@@ -1,16 +1,11 @@
 import { environmentVariables } from '../utils/constants.js'
 
 export default class Git {
-
     async getUser ( user ) {
         const profileUser = await fetch( `https://api.github.com/users/${user}?client_id=${environmentVariables.clientId}&client_secret=${environmentVariables.clientSecret}` );
         const userRepos = await fetch( `https://api.github.com/users/${user}/repos?per_page=${environmentVariables.reposCount}&sort=${environmentVariables.reposScort}client_id=${environmentVariables.clientId}&client_secret=${environmentVariables.clientSecret}` )
         const profile = await profileUser.json()
         const repos = await userRepos.json()
         return { profile, repos }
-
     }
-
-
-
 }
